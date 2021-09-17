272ba8efe7acdc803b1672a4ee006df3084293b1
Harout <harmardir@gmail.com>
2021-09-17 12:13:41 +0300

    Creating view function

--- fortune_teller/fortuneteller/randomfortune/views.py
+++ fortune_teller/fortuneteller/randomfortune/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 01- Define a function that takes one parameter request and returns render function with 2 parameters request and path
+
+
+def fortune(request):
+    return render(request,"randomfortune/fortune.html")
