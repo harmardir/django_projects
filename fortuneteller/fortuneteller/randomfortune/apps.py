bcbd1a79deb78cce643d4f40470a08c87b99673b
Harout <harmardir@gmail.com>
2021-09-17 11:32:25 +0300

    deleting old files

--- fortuneteller/fortuneteller/randomfortune/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RandomfortuneConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'randomfortune'
--- quote_generator/quotegenerator/randomquote/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def quote(request):
-    return render(request,"randomquote/quote.html")
