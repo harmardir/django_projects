e370a867ae880989f6365b339305ce8fcea34a02
Harout Mardirossian <harmardir@gmail.com>
2022-04-01 09:27:44 +0300

    Creating the django project and the app

---
+++ data_query/bookstore/main/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.db.models import Q
+
+from .models import Book , Author , Publisher
+
+def setup(request):
+    pass
+
+def index(request):
+    pass
